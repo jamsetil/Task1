6e6ecf6ab30c413b23c88a7b8bf3eb15b4ca1c78
ilyas <iazizzade64@gmail.com>
2024-02-06 15:02:12 +0400

    updates

---
+++ src/main/java/org/abbtech/Task3/task5/DayOfWeek.java
@@ -0,0 +1,42 @@
+package org.abbtech.Task3.task5;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class DayChecker{
+    public static void main(String[] args) {
+        DayOfWeek dayOfWeek = DayOfWeek.SATURDAY;
+        System.out.println(dayOfWeek.isWeekend(dayOfWeek));
+    }
+}
+
+
+
+public enum DayOfWeek {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
+
+    boolean isWeekend(DayOfWeek dayOfWeek){
+        switch (dayOfWeek){
+            case SATURDAY ->{
+                return true;
+            }
+            case SUNDAY -> {
+            return true;
+            }
+            default -> {
+                return false;
+            }
+        }
+    };
+}
